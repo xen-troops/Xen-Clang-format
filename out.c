0d2d9db0d989401921351f919487196106dbe9dd
Ishani Chugh <chugh.ishani@research.iiit.ac.in>
2017-04-06 22:51:00 +0530

    Braces in newline done

---
+++ out.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int y;
+    char *p= "adsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+             "aaaaaaaaaaaaaaaaaaaaaa";
+    for (;;)
+    {
+        int z= 5;
+    }
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	int y;
+	char *p = "adsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	for(;;)
+		{
+		int z = 5;
+}
+	return 0;
+}
